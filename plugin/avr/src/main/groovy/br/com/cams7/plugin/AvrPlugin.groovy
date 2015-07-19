ea67f63ad4a4f2b50a4a7e4b132e196ec224c727
“Cesar <ceanma@gmail.com>
2015-07-19 18:45:50 -0300

    Foi criado a classe 'AvrPlugin' no plugin 'avr'

---
+++ plugin/avr/src/main/groovy/br/com/cams7/plugin/AvrPlugin.groovy
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.cams7.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author cams7
+ *
+ */
+class AvrPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+	}
+}
