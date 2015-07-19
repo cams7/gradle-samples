8639ac3be7ab56fac2ccdc79287bd4268ee91115
“Cesar <ceanma@gmail.com>
2015-07-19 19:45:31 -0300

    O plugin 'avr' e uma copia do plugin 'c'

--- plugin/avr/src/main/groovy/br/com/cams7/plugin/AvrPlugin.groovy
+++ plugin/avr/src/main/groovy/br/com/cams7/plugin/AvrPlugin.groovy
@@ -3,16 +3,24 @@
  */
 package br.com.cams7.plugin
 
+import org.gradle.api.Incubating
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.language.c.plugins.CLangPCHPlugin
+import org.gradle.language.c.plugins.CLangPlugin
+import org.gradle.nativeplatform.plugins.NativeComponentPlugin
 
 /**
  * @author cams7
  *
  */
+@Incubating
 class AvrPlugin implements Plugin<Project> {
 
 	@Override
 	public void apply(Project project) {
+		project.getPluginManager().apply(NativeComponentPlugin.class)
+		project.getPluginManager().apply(CLangPlugin.class)
+		project.getPluginManager().apply(CLangPCHPlugin.class)
 	}
 }
